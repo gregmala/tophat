7ae5e650bd3891b5a65d2ee9a969a9f29b15e3be
pierre jebara <pierrehenrijebara@gmail.com>
2023-02-28 10:13:46 +0100

    products routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,40 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    @unique_categories = Product.distinct.pluck(:category)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user = current_user
+    @product.save
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product.find(params[:id])
+    @product.update(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:brand,:color, :size, :apparel_type, :category, :price_per_cycle, :description)
+  end
 end
