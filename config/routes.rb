0d388d6aaa2e1324c0dc17e444e7cb3739b9592d
pierre jebara <pierrehenrijebara@gmail.com>
2023-02-28 18:00:39 +0100

    completed a lot

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "products#index"
   resources :users, only: [:show]
-  resources :products do
+  resources :products,  only: [:show] do
     resources :bookings , only: [:new , :create]
+  end
+  resources :products, only: [:index] do
+    collection do
+      get "/category/:category/filters/:array", to: 'products#filter', as: 'filter'
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
